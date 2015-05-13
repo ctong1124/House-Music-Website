10cd2ab2ace13d9567544e6bd2150496dadf6884
Carisa Tong <ctong1124@gmail.com>
2015-05-12 20:00:19 -0400

    Setting up git to github p1

---
+++ about.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<?php $thisPage="about"; ?>
+<?php $section="main"; ?>
+<html class="no-js"> <!--<![endif]-->
+    <?php include "phpincludes/head.php"; ?>
+
+    
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        
+
+
+        <?php include "phpincludes/nav.php"; ?>
+
+        <div id="main">
+        <br>
+        <br>
+        <div class="about scene_element scene_element--fadeindown">
+          <div class="section pad-medium-top">
+          <div class="section-fixed "> 
+            <div class="col12">
+              <h3>about</h3>
+            </div>
+          </div>
+        </div>
+          <div class="section pad-small-top">
+          <div class="section-fixed ">
+            <div class="col12">
+              <p>
+                A Guide to House Music is the work of <a href="http://www.carisatong.com" target="blank">Carisa Tong</a>
+                for Northeastern University's undegraduate Web Design and Development course (IM2400). This site
+                would not be possible without the information from these sources
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="section pad-small">
+          <div class="section-fixed ">
+            <div class="col12 sources">
+              <div class="sans-serif-p">
+                <a href="http://techno.org/electronic-music-guide/" target="blank">Ishkur's Guide to Electronic Music</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://en.wikipedia.org/wiki/House_music" target="blank">Wikipedia House Music Page</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://youtu.be/Y1HpY65cXDA" target="blank">Pump Up the Volume (Channel 4's Documentary on House Music)</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="https://www.youtube.com/watch?v=L7JHqtMuuAI" target="blank">Mixmag's Origins of House Documentary</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://www.spin.com/articles/40-best-deep-house-tracks-list/" target="blank">Spin's 40 Best Deep House Tracks</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://music.hyperreal.org/library/history_of_house.html" target="blank">Hyperreal Music's History of House</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://www.univie.ac.at/Anglistik/webprojects/LiveMiss/Chicago-House/house-text.htm" target="blank">The Beginnings of House Music</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://www.diffen.com/difference/House_Music_vs_Techno_Music" target="blank">Diffen's House Music vs. Techno Music</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://blog.kexp.org/2011/11/10/kexp-documentaries-electronic-innovators-the-belleville-three/" target="blank">KEXP Documentaries: “Electronic Innovators” – The Belleville Three</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://en.wikipedia.org/wiki/Kevin_Saunderson" target="blank">Wikipedia Kevin Saunderson Page</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://en.wikipedia.org/wiki/Kevin_Saunderson" target="blank">Wikipedia Kevin Saunderson Page</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://en.wikipedia.org/wiki/Juan_Atkins" target="blank">Wikipedia Juan Atkins Page</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://en.wikipedia.org/wiki/Derrick_May(musician)" target="blank">Wikipedia Derrick May Page</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://en.wikipedia.org/wiki/Madchester" target="blank">Wikipedia Madchester Page</a>
+              </div>
+              <div class="sans-serif-p">
+                <a href="http://hardcorewillneverdie.com/Flash/historyHWND.html" target="blank">HardcoreWillNeverDie's Rave Timeline </a>
+              </div>
+
+              
+            </div>
+          </div>
+          </div>
+          <br><br>
+          <div class="section pad-large-bot">
+          <div class="section-fixed">
+            <div class="col12">
+              <p>
+                Please let me know of any things that need to be updated/corrected/general feedback!
+              </p>
+            </div>
+          </div>
+        </div>
+
+          <div class="section pad-medium form">
+          <div class="section-fixed ">
+            <form name="htmlform" method="post" action="html_form_send.php">
+              <table>
+              </tr>
+              <tr>
+               <td valign="top">
+                <label for="first_name">Name</label>
+              </td>
+              <td valign="top">
+                <input  type="text" name="first_name" maxlength="50" size="30">
+              </td>
+            </tr>
+            <tr>
+             <td valign="top">
+              <label for="email">Email Address</label>
+            </td>
+            <td valign="top">
+              <input  type="text" name="email" maxlength="80" size="30">
+            </td>
+
+            </tr>
+           
+            <tr>
+             <td valign="top">
+              <label for="comments"></label>
+            </td>
+            <td valign="top">
+              <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+            </td>
+
+            </tr>
+            <tr>
+             <td colspan="2" style="text-align:center">
+              <!-- We are grateful to you for keeping this link in place. thank you. -->
+              <input type="submit" value="Submit">   ( <a href="http://www.freecontactform.com/html_form.php">HTML Form</a> )
+            </td>
+            </tr>
+            </table>
+            </form>
+          </div>
+
+        </div>
+        </div>
+        </div>
+
+        <?php include "phpincludes/html_form_send.php"; ?>
+
+
+
+
+        
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
