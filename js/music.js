a06bd7dc0f3e9e8a01ee87640d67f634fe3d5c24
Carisa Tong <ctong1124@gmail.com>
2016-01-14 11:15:49 -0800

    um replacing from carisatong.com

--- js/music.js
+++ js/music.js
@@ -5,7 +5,7 @@ var data={
             "song": "Your Love",
             "year": 1984,
             "descrip": "&quotYour Love&quot was one of the first popular tracks in this new house genre. It was first played by Frankie Knuckles and quickly spread throughout the rest of the Chicago clubs. In 1984, it was only available to DJs as a copy of a tape, but was then released in 1986 as a 12” vinyl by Persona Records.",
-            "page": "early1980s",
+            "page": "musicearly1980s1",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/LOLE1YE_oFQ\" frameborder=\"0\" allowfullscreen><\/iframe>"
         },
         {
@@ -13,7 +13,7 @@ var data={
             "song": "On and On",
             "year":1984,
             "descrip": "“On and On” is widely regarded as the first house record on vinyl. There were only 500 records printed when it was first released. ",
-            "page": "early1980s",
+            "page": "musicearly1980s1",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/qUeMFG4wjJw\" frameborder=\"0\" allowfullscreen><\/iframe>"
         },
         {
@@ -21,7 +21,7 @@ var data={
             "song": "Music is the Key",
             "year":1985,
             "descrip": "“Music is the Key” was the first house song to make the music charts when it hit #9 on the US Dance Charts in 1985",
-            "page": "early1980s",
+            "page": "musicearly1980s1",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/QRsasda3aNY\" frameborder=\"0\" allowfullscreen><\/iframe>"
         },
         {
@@ -37,7 +37,7 @@ var data={
             "song": "Strings of Life",
             "year":1987,
             "descrip": "Rhythim is Rhythim was the moniker of Derrick Mays. “Strings of Life” was considered a classic in the house and techno genre. It became wildly popular a couple years after its original pressing and was released several more times.",
-            "page": "detroit",
+            "page": "musicdetroit1",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/qiCEGXGm-z0\" frameborder=\"0\" allowfullscreen><\/iframe>"
         },
         {
@@ -45,15 +45,23 @@ var data={
             "song": "Big Fun",
             "year":1988,
             "descrip": "Inner City was the work of Kevin Saunderson and Paris Grey. “Big Fun” did well on the US charts but blew up overseas in the UK. It was the first Detroit techno song to reach the UK and set off a huge house and techno interest overseas.",
-            "page": "detroit",
+            "page": "musicdetroit1",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/omfiVkkJ1OU\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Farley Jackmaster Funk, featuring Darryl Pandy",
+            "song": "Love Can't Turn Around",
+            "year":1986,
+            "descrip": "",
+            "page": "musicuk3",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/Lr-OgG1A74c\" frameborder=\"0\" allowfullscreen><\/iframe>"
         },
         {
             "artist": "Joe Smooth",
             "song": "Promised Land",
             "year":1987,
             "descrip": "The lyrics of “Promised Land” were based off a Martin Luther King Jr. speech. Released in 1987, it peaked at #56 in the UK Singles Chart but a cover of it by the Style Council reached the Top 40.",
-            "page": "uk",
+            "page": "musicuk1",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/ML3w4HIMHpY\" frameborder=\"0\" allowfullscreen><\/iframe>"
         },
         {
@@ -61,7 +69,7 @@ var data={
             "song": "That's The Way Love Is",
             "year":1989,
             "descrip": "Ten City was vocalist Byron Stingily, guitarist Herb Lawson, keyboardist Byron Burke and producer Marshall Jefferson. “That's the Way Love Is” is a house anthem still heard today and reached number eight in the UK when released.",
-            "page": "uk",
+            "page": "musicuk1",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/Y_edhEpE_YM\" frameborder=\"0\" allowfullscreen><\/iframe>"
         },
         {
@@ -69,9 +77,34 @@ var data={
             "song": "Voodoo Ray",
             "year":1988,
             "descrip": "“Voodoo Ray” got its start at the Hacienda and was written by a Hacienda regular Gerald Simpson. The groove reached number 12 on the UK singles chart.",
-            "page": "uk",
+            "page": "musicuk1",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/ivr57dcs9-E\" frameborder=\"0\" allowfullscreen><\/iframe>"
         },
+        {
+            "artist": "Happy Mondays",
+            "song": "Wrote For Luck (Think About The Future Mix)",
+            "year":1988,
+            "descrip": "Manchester's The Happy Mondays started out as an indie band, but faced a shift as they recorded their second album <span style=\"font-style: italic\" >Bummed</span>. They brought on DJ Paul Oakenfold to give the record a rave/club feel&mdash;the first rock band in the UK to embrace the emerging culture.",
+            "page": "musicuk2",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/7fWYtIdJD7M\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "The Orb",
+            "song": "Little Fluffy Clouds",
+            "year":1990,
+            "descrip": "“Little Fluffy Clouds” was one of the first songs to emerge from the ambient house genre, a chilled out version of what was happening in the rave scene. It features an anecdote from an interview with musician Rickie Lee Jones over a looping electronic beat.",
+            "page": "musicuk2",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/CdMs7eqMvNg\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Shut Up and Dance",
+            "song": "10 Pounds to Get In",
+            "year":1989,
+            "descrip": "This song was part of the beginnings of the breakbeat genre. Shut Up and Dance used sped-up hip-hop-like breakbeats instead drum machines to create this new sound for the rave era.",
+            "page": "musicuk2",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/kUIPr9eIqU8\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+
         {
             "artist": "E-Zee Possee & MC Kinky",
             "song": "Everything Starts With An 'E'",
@@ -111,6 +144,62 @@ var data={
             "descrip": "T-Coy was the work of Manchester's Mike Pickering, Ritchie Close and Simon Topping.",
             "page": "none",
             "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/97vO3qvuQqc\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Joey Beltram",
+            "song": "Energy Flash",
+            "year":1990,
+            "descrip": "“Energy Flash” was the work of Queens producer Joey Beltram. It featured a much darker sound than typical House songs but had much success in the UK rave scene.",
+            "page": "none",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/4lOb799cTxM\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Madonna",
+            "song": "Vogue",
+            "year":1990,
+            "descrip": "Madonna's “Vogue” was considered the first pop song inspired by House to top the US charts and bring House to the mainstream.",
+            "page": "music1990s1",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/GuJQSAiODqI\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Leftfield",
+            "song": "Open Up",
+            "year":1995,
+            "descrip": "“Open Up” is a song recorded by Leftfield featuring John Lydon of Sex Pistols. The single reached #13 in the UK Singles Chart and was significant because it featured such an important figure in punk history.",
+            "page": "music1990s1",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/oYNeQ4i7Ne8\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Tori Amos",
+            "song": "Professional Widow (Armand Van Helden Remix)",
+            "year":1996,
+            "descrip": "Armand Van Helden's remix of “Professional Widow” marked one of the first remixes of a pop song to make it big in the mainstream and the clubs.",
+            "page": "music1990s1",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/TYuKMnjZu_s\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Daft Punk",
+            "song": "Revolution 909",
+            "year":1997,
+            "descrip": "“Revoluion 909” was part of Daft Punk's debut album Homework. Homework charted in 14 different countries, peaking at number 3 on the French Albums Chart, number 150 on the United States Billboard 200 and at number 8 on the UK Albums Chart.",
+            "page": "music1990s2",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/uURB-vo9rZ4\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Stardust",
+            "song": "Music Sounds Better With You",
+            "year":1998,
+            "descrip": "“Stardust” was the moniker of producers Alax Braxe, Daft Punk’s Thomas Bangalter and vocalist Benjamin Diamond. “Music Sounds Better With You” was the only song produced by the group.",
+            "page": "music1990s2",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/NxBdUIlhxo8\" frameborder=\"0\" allowfullscreen><\/iframe>"
+        },
+        {
+            "artist": "Cassius",
+            "song": "1999",
+            "year":1999,
+            "descrip": "",
+            "page": "music1990s2",
+            "embed": "<iframe width=\"420\" height=\"315\" src=\"//www.youtube.com/embed/K7iXd2cHD44\" frameborder=\"0\" allowfullscreen><\/iframe>"
         }
 ]}
 
@@ -164,32 +253,44 @@ function listenHTML(current) {
 $(document).ready(function() {
 
     //CREATING MUSIC CONTENT WITHIN TIMELINE PAGES
-    if (document.getElementById("music") != null) {
-        var div = document.getElementById("dom-target-page");
-        var page = div.textContent;
+    if (document.getElementById("dom-target-page") != null) { 
         
 
-        var output="";
-            for (var i in data.music) {
-                if(data.music[i].page == page) {
-                    output+= "<div class=\"section pad-small\"><div class=\"section-fixed\"><div class=\"col8\"><div class=\"videoWrapper\">";
-                    output+= data.music[i].embed;
-                    output+= "</div></div><div class=\"col4\"><div class=\"sans-serif-p\"><span class=\"headline-p\">Artist <\/span>";
-                    output+= data.music[i].artist;
-                    output+= "<\/div> <div class=\"sans-serif-p\"><span class=\"headline-p\">Song <\/span>";
-                    output+= data.music[i].song;
-                    output+= "</div> <div class=\"sans-serif-p\"><span class=\"headline-p\">Year <\/span>";
-                    output+= data.music[i].year;
-                    output+= "<\/div><div class=\"sans-serif-p\">";
-                    output+= data.music[i].descrip;
-                    output+= "<\/div><\/div><\/div><\/div>";
-                }
+        //unique list of vid list IDs 
+        var musicVideoIDs = [];
+        for (var i in data.music) {
+            musicVideoIDs[i] = data.music[i].page;
+        }
+        musicVideoIDs = $.unique(musicVideoIDs);
+
+
+        for (k=0; k < musicVideoIDs.length; k++) { 
+
+            if (document.getElementById(musicVideoIDs[k])) {
+                var output="";
+                    for (var i in data.music) {
+                        if(data.music[i].page == musicVideoIDs[k]) {
+                            output+= "<div class=\"section pad-small\"><div class=\"section-fixed\"><div class=\"col8\"><div class=\"videoWrapper\">";
+                            output+= data.music[i].embed;
+                            output+= "</div></div><div class=\"col4\"><div class=\"sans-serif-p\"><span class=\"headline-p\">Artist <\/span>";
+                            output+= data.music[i].artist;
+                            output+= "<\/div> <div class=\"sans-serif-p\"><span class=\"headline-p\">Song <\/span>";
+                            output+= data.music[i].song;
+                            output+= "</div> <div class=\"sans-serif-p\"><span class=\"headline-p\">Year <\/span>";
+                            output+= data.music[i].year;
+                            output+= "<\/div><div class=\"sans-serif-p\">";
+                            output+= data.music[i].descrip;
+                            output+= "<\/div><\/div><\/div><\/div>";
+                        }
+                    }
+
+                document.getElementById(musicVideoIDs[k]).innerHTML=output;
             }
-
-        document.getElementById("music").innerHTML=output;
-
-        
+        }
+    
     }
+
+
 
     if (document.getElementById("listen") != null) {
         var arrayRandNums = [];
